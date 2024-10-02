bb929c9e6dddb797d9c8cae1104e07e0a87db145
Joel Carrera <carrerajgo@gmail.com>
2024-10-01 20:19:19 -0500

    Init Project

---
+++ src/main/java/pe/chirasoft/training/students/StudentsApplication.java
@@ -0,0 +1,13 @@
+package pe.chirasoft.training.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/chirasoft/training/students/StudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.chirasoft.training.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
